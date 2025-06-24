40acfbaf594ffa38a623fb155704951010626829
JuanDiPosada <juandiegocifuentesposada@gmail.com>
2025-06-24 12:46:05 -0500

    feat:colores default

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 module.exports = {
   content: [
     "./public/**/*.html",
-    "./src/**/*.css"        // Escanea clases BEM en tus archivos CSS
+    "./src/**/*.{css,js}"       // Escanea clases BEM en tus archivos CSS
   ], // Ruta a tus archivos HTML
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        azulPotente: '#063E67', 
+        azulSecundario: '#4690FF', 
+        textosNoNegro: '#393434', 
+        borde1: '#C8C8C8',
+        borde2: '#B8B4B4',
+        background: '#F2F8FF',
+      },     
+    },
   },
   plugins: [],
 };
